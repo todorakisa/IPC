379c297106b32b1bab04e788db8701ca439c258d
todorakisa <todor.domitrov2000@gmail.com>
2017-12-11 19:44:10 +0200

    Update cyclicRead.c

--- cyclicRead.c
+++ cyclicRead.c
@@ -26,11 +26,21 @@ int main()
 	}
     
     uint64_t position = memory->position;
-    uint32_t seed;
+    uint32_t seed = 0;
+    uint32_t true_seed = seed;
 	printf("starting at %ld\n", position);
+    int i=0;
     while(true){
-        while(memory -> position != position){
-            printf("Veryfy array[%ld] - seed:%ld\n",position,verify((void *)memory->array[position]));
+        while(memory -> position != position)
+	    seed = verify((void *)memory->array[position]);
+	    if(i == 0){
+		    true_seed = seed;
+    	    }else if(true_seed + 1 != seed){
+            	printf("Error");
+	    }
+	    true_seed++;
+	    i = 1;
+            printf("Veryfy array[%ld] - seed:%ld\n",position,seed);
             position ++;
             position %=COUNT;
         }
